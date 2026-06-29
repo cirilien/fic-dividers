e10ab701d12828149dcc81026f49d3a5bfa8404e
cirilien <accio.cirilien@gmail.com>
2026-06-29 14:57:19 +1000

    biome

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useRef, useEffect } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./App.css";
 
 const DEFAULT_HTML = "<hr />";
@@ -54,36 +54,45 @@ function App() {
         <div className="preview-container">
           <div class="preview-content" id="workskin">
             <p>
-              Xue Yang presses a hand to the cut on his shoulder, and his fingers come
-              back sticky with blood. “Fuck,” he mutters. But he doesn’t get time to dwell
-              on the way his heart is racing or his lips are tingling, because there’s a
-              depressingly familiar yell of recognition followed by pounding footsteps.
+              Xue Yang presses a hand to the cut on his shoulder, and his
+              fingers come back sticky with blood. “Fuck,” he mutters. But he
+              doesn&rsquo;t get time to dwell on the way his heart is racing or
+              his lips are tingling, because there&rsquo;s a depressingly
+              familiar yell of recognition followed by pounding footsteps.
             </p>
-            <p>And just like that, he’s on the run again.</p>
+            <p>And just like that, he&rsquo;s on the run again.</p>
+            {/* biome-ignore lint: kinda have to here */}
             <div dangerouslySetInnerHTML={{ __html: html }} />
             <p>
-              It takes two weeks before he cracks. Two weeks of close calls and crawling
-              through the pollution-clogged arteries of the capital only to come to the
-              bitter realisation that Jin Guangshan isn’t going to give up on killing him.
-              And after that, it’s another week of lurking in a district of the city he
-              knows Song Lan sometimes passes through before he finds him. Buying produce,
-              of all fucking things.
+              It takes two weeks before he cracks. Two weeks of close calls and
+              crawling through the pollution-clogged arteries of the capital
+              only to come to the bitter realisation that Jin Guangshan
+              isn&rsquo;t going to give up on killing him. And after that,
+              it&rsquo;s another week of lurking in a district of the city he
+              knows Song Lan sometimes passes through before he finds him.
+              Buying produce, of all fucking things.
             </p>
           </div>
         </div>
       </div>
       <div className="editor-panel">
         <div className="editor-group">
-          <label className="panel-label">HTML of the divider element</label>
+          <label htmlFor="html" className="panel-label">
+            HTML of the divider element
+          </label>
           <textarea
+            id="html"
             value={html}
             onChange={(e) => setHtml(e.target.value)}
             spellCheck={false}
           />
         </div>
         <div className="editor-group">
-          <label className="panel-label">CSS</label>
+          <label htmlFor="css" className="panel-label">
+            CSS
+          </label>
           <textarea
+            id="css"
             value={css}
             onChange={(e) => setCss(e.target.value)}
             spellCheck={false}
